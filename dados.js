74bf4b75a45835ca3da65ce076c6667e3f9d0cbb
Gustavo Vieira <Gustavos3654@gmail.com>
2024-03-16 01:25:04 -0300

    fix: deploy

---
+++ dados.js
@@ -0,0 +1,39 @@
+const imageUrlsPrada = [];
+for (let i = 1; i <= 86; i++) {
+    const url = `./images/prada/${i}.png`;
+    imageUrlsPrada.push(url);
+}
+const captionsPrada = [];
+for (let i = 1; i <= 86; i++) {
+    captionsPrada.push("Prada");
+}
+
+const imageUrlsFendi = [];
+for (let i = 1; i <= 56; i++) {
+    const url = `./images/fendi/${i}.png`;
+    imageUrlsFendi.push(url);
+}
+const captionsFendi = [];
+for (let i = 1; i <= 56; i++) {
+    captionsFendi.push("Fendi");
+}
+
+const imageUrlsBalenciaga = [];
+for (let i = 1; i <= 86; i++) {
+    const url = `./images/balenciaga/${i}.png`;
+    imageUrlsBalenciaga.push(url);
+}
+const captionsBalenciaga = [];
+for (let i = 1; i <= 86; i++) {
+    captionsBalenciaga.push("Balenciaga");
+}
+
+const imageUrlsValentino = [];
+for (let i = 1; i <= 57; i++) {
+    const url = `./images/valentino/${i}.png`;
+    imageUrlsValentino.push(url);
+}
+const captionsValentino = [];
+for (let i = 1; i <= 57; i++) {
+    captionsValentino.push("Valentino");
+}
