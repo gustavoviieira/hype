d87c729263b8b103d4acedab89ff46e699517267
Gustavo Vieira <Gustavos3654@gmail.com>
2024-03-18 21:34:43 -0300

    Update: news files

--- dados.js
+++ dados.js
@@ -1,3 +1,4 @@
+// Prada
 const imageUrlsPrada = [];
 for (let i = 1; i <= 86; i++) {
     const url = `./images/prada/${i}.png`;
@@ -8,6 +9,7 @@ for (let i = 1; i <= 86; i++) {
     captionsPrada.push("Prada");
 }
 
+// Fendi
 const imageUrlsFendi = [];
 for (let i = 1; i <= 56; i++) {
     const url = `./images/fendi/${i}.png`;
@@ -18,6 +20,7 @@ for (let i = 1; i <= 56; i++) {
     captionsFendi.push("Fendi");
 }
 
+// Balenciaga
 const imageUrlsBalenciaga = [];
 for (let i = 1; i <= 86; i++) {
     const url = `./images/balenciaga/${i}.png`;
@@ -28,6 +31,7 @@ for (let i = 1; i <= 86; i++) {
     captionsBalenciaga.push("Balenciaga");
 }
 
+// Valentino
 const imageUrlsValentino = [];
 for (let i = 1; i <= 57; i++) {
     const url = `./images/valentino/${i}.png`;
@@ -36,4 +40,37 @@ for (let i = 1; i <= 57; i++) {
 const captionsValentino = [];
 for (let i = 1; i <= 57; i++) {
     captionsValentino.push("Valentino");
+}
+
+//YSL
+const imageUrlsYSL = [];
+for (let i = 1; i <= 54; i++) {
+    const url = `./images/ysl/${i}.png`;
+    imageUrlsYSL.push(url);
+}
+const captionsYSL = [];
+for (let i = 1; i <= 54; i++) {
+    captionsYSL.push("YSL");
+}
+
+// Dior
+const imageUrlsDior = [];
+for (let i = 1; i <= 63; i++) {
+    const url = `./images/dior/${i}.png`;
+    imageUrlsDior.push(url);
+}
+const captionsDior = [];
+for (let i = 1; i <= 63; i++) {
+    captionsDior.push("Dior");
+}
+
+// LouisVuitton
+const imageUrlsLouisVuitton = [];
+for (let i = 2; i <= 92; i++) {
+    const url = `./images/louisVuitton/${i}.png`;
+    imageUrlsLouisVuitton.push(url);
+}
+const captionsLouisVuitton = [];
+for (let i = 1; i <= 92; i++) {
+    captionsLouisVuitton.push("Louis Vuitton");
 }
---
+++ scriptYSL.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const gallery = document.querySelector(".gallery");
+
+function createImageLink(imageUrl, caption) {
+    const link = document.createElement("a");
+    link.href = imageUrl;
+    link.dataset.lightbox = "models"; // Assuming you have a lightbox library
+    if (caption) {
+        link.dataset.title = caption;
+    }
+
+    const image = document.createElement("img");
+    image.src = imageUrl;
+
+    link.appendChild(image);
+    return link;
+}
+
+for (let i = 0; i < imageUrlsYSL.length; i++) {
+    const imageLink = createImageLink(imageUrlsYSL[i], captionsYSL[i] || null);
+    gallery.appendChild(imageLink);
+    container.append(gallery);
+}
